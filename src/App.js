782776dc852ac65eb7e7311accc87d06c8a55d70
BENLKAMLENourdine <benlkamle.tiznit.95@gmail.com>
2019-10-18 05:33:28 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const link = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY"
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  async componentDidMount() {
+    const res = (await axios.get(link))
+    console.log(res)
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
 }
 
 export default App;
